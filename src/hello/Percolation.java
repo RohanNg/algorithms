181a76d7e002c8cf34a8097600bcb522657c87d6
Nguyen Hai Dang <nguyen.h.dang.1001@gmail.com>
2017-01-27 10:39:43 +0200

    remove publicates

--- src/hello/Percolation.java
+++
@@ -1,8 +0,0 @@
-package percolation;
-
-public interface Percolation {
-	void open(int row, int col);
-	boolean isOpen(int row, int col);
-	boolean isFull(int row, int col);
-	boolean percolates();
-}
--- src/percolation/SayHello.java
+++
@@ -1,7 +0,0 @@
-package percolation;
-
-public class SayHello {
-	public static void main(String[] args){
-		System.out.println("Hello world!");
-	}
-}
